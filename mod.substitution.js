27822fe0d53250632bbe306ccce67d86f692dad9
Gero Schembri <gero.schembri@gmail.com>
2026-03-28 13:08:54 +0100

    feat(substitution): build unified substitutions page without iframe

--- mod.substitution.js
+++ mod.substitution.js
@@ -2,12 +2,46 @@ var Mod = Mod || {};
 
 Mod.Substitution = {
   handlePage: function () {
-    return this.handleDaily();
+    var actor = ROOMS_APP.Auth.getUserContext();
+    var dailyBootstrap = this.buildDailyBootstrap_(actor);
+    var dailyHtml = this.renderTemplateToString_('ui.substitution.daily', {
+      pageTitle: 'Sostituzioni giornaliere',
+      initialModelJson: JSON.stringify(dailyBootstrap),
+      initialRoomId: ''
+    });
+    var reportsHtml = this.renderTemplateToString_('ui.substitution.reports', {
+      pageTitle: 'Report sostituzioni',
+      viewerMode: 'list',
+      viewerEmbedded: true,
+      viewerUser: actor,
+      reports: ROOMS_APP.Replacements.listVisibleArchivedReports_(),
+      report: null
+    });
+
+    return renderTemplate_('ui.substitution', {
+      pageTitle: 'Sostituzioni',
+      initialModelJson: JSON.stringify({
+        title: 'Sostituzioni',
+        subtitle: 'Modulo unificato delle sostituzioni.',
+        user: actor
+      }),
+      dailyContentHtml: this.extractFragment_(dailyHtml, 'SUBSTITUTION_DAILY_CONTENT_START', 'SUBSTITUTION_DAILY_CONTENT_END'),
+      dailyScriptsHtml: this.extractFragment_(dailyHtml, 'SUBSTITUTION_DAILY_SCRIPTS_START', 'SUBSTITUTION_DAILY_SCRIPTS_END'),
+      reportsHtml: this.extractFragment_(reportsHtml, 'SUBSTITUTION_REPORTS_FRAGMENT_START', 'SUBSTITUTION_REPORTS_FRAGMENT_END')
+    });
   },
 
   handleDaily: function () {
     var actor = ROOMS_APP.Auth.getUserContext();
-    var bootstrap = {
+    return renderTemplate_('ui.substitution.daily', {
+      pageTitle: 'Sostituzioni giornaliere',
+      initialModelJson: JSON.stringify(this.buildDailyBootstrap_(actor)),
+      initialRoomId: ''
+    });
+  },
+
+  buildDailyBootstrap_: function (actor) {
+    return {
       generatedAtISO: '',
       date: '',
       refreshSec: 60,
@@ -23,11 +57,25 @@ Mod.Substitution = {
       branchLabels: {},
       pages: []
     };
+  },
 
-    return renderTemplate_('ui.substitution.daily', {
-      pageTitle: 'Sostituzioni giornaliere',
-      initialModelJson: JSON.stringify(bootstrap),
-      initialRoomId: ''
+  renderTemplateToString_: function (filename, viewModel) {
+    var template = HtmlService.createTemplateFromFile(filename);
+    Object.keys(viewModel || {}).forEach(function (key) {
+      template[key] = viewModel[key];
     });
+    return template.evaluate().getContent();
+  },
+
+  extractFragment_: function (html, startMarker, endMarker) {
+    var source = String(html || '');
+    var startToken = '<!-- ' + startMarker + ' -->';
+    var endToken = '<!-- ' + endMarker + ' -->';
+    var startIndex = source.indexOf(startToken);
+    var endIndex = source.indexOf(endToken);
+    if (startIndex < 0 || endIndex < 0 || endIndex < startIndex) {
+      return '';
+    }
+    return source.slice(startIndex + startToken.length, endIndex);
   }
 };
